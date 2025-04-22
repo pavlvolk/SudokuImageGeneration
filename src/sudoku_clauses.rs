3b6491ead354d18c009a47a27f7c438c4f1c16d3
paul <paulvolk.pv@gmail.com>
2025-04-22 21:52:10 +0200

    One number each clauses implemented

--- src/sudoku_clauses.rs
+++ src/sudoku_clauses.rs
@@ -1,15 +1,15 @@
-pub fn sudoku_clauses(board_size: usize) -> Vec<Vec<i16>> {
+pub fn sudoku_clauses(board_size: i16) -> Vec<Vec<i16>> {
     let mut clauses = vec![vec![]];
 
     clauses
 }
 
-pub fn one_number_each(clauses: &mut [i16], board_size: usize) {
+pub fn one_number_each(clauses: &mut [i16], board_size: i16) {
     //at least one number
-    for r in 1..board_size {
-        for c in 1..board_size {
-            let mut cell_clauses:Vec<usize> = vec![];
-            for val in 1..board_size {
+    for r in 1..=board_size {
+        for c in 1..=board_size {
+            let mut cell_clauses:Vec<i16> = vec![];
+            for val in 1..=board_size {
                 cell_clauses.push(var_num(r, c, val, board_size))
             }
             clauses.push(cell_clauses);
@@ -17,9 +17,17 @@ pub fn one_number_each(clauses: &mut [i16], board_size: usize) {
     }
 
     //At most one number
-
+    for r in 1..=board_size {
+        for c in 1..=board_size {
+            for val1 in 1..board_size {
+                for val2 in val1+1..=board_size {
+                    clauses.push(vec![-var_num(r, c, val1, board_size), -var_num(r, c, val2, board_size)])
+                }
+            }
+        }
+    }
 }
 
-fn var_num(row: usize, column: usize, val: usize, board_size: usize) -> usize{
+fn var_num(row: i16, column: i16, val: i16, board_size: i16) -> i16{
     (row-1) * board_size * board_size + (column-1) * board_size + val
 }
