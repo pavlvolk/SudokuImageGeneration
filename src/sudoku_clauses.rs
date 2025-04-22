845a240e0e88dd72d97fef35b5f6b5cc2d809757
paul <paulvolk.pv@gmail.com>
2025-04-22 22:02:32 +0200

    Row and column clauses implemented

--- src/sudoku_clauses.rs
+++ src/sudoku_clauses.rs
@@ -28,6 +28,56 @@ pub fn one_number_each(clauses: &mut [i16], board_size: i16) {
     }
 }
 
+pub fn add_row_clauses(clauses: &mut [i16], board_size: i16) {
+    //at least every number in each row
+    for r in 1..=board_size {
+        for val in 1..=board_size {
+            let mut row_clauses:Vec<i16> = vec![];
+            for c in 1..=board_size {
+                row_clauses.push(var_num(r, c, val, board_size))
+            }
+            clauses.push(row_clauses);
+        }
+    }
+
+    //at most every number per row
+    for r in 1..=board_size {
+        for val in 1..=board_size {
+            for c1 in 1..board_size {
+                for c2 in c1+1..=board_size {
+                    clauses.push(vec![-var_num(r, c1, val, board_size), -var_num(r, c2, val, board_size)])
+                }
+            }
+        }
+    }
+}
+
+pub fn add_column_clauses(clauses: &mut [i16], board_size: i16) {
+    //at least every number in each row
+    for c in 1..=board_size {
+        for val in 1..=board_size {
+            let mut row_clauses:Vec<i16> = vec![];
+            for r in 1..=board_size {
+                row_clauses.push(var_num(r, c, val, board_size))
+            }
+            clauses.push(row_clauses);
+        }
+    }
+
+    //at most every number per row
+    for c in 1..=board_size {
+        for val in 1..=board_size {
+            for r1 in 1..board_size {
+                for r2 in r1+1..=board_size {
+                    clauses.push(vec![-var_num(r1, c, val, board_size), -var_num(r2, c, val, board_size)])
+                }
+            }
+        }
+    }
+}
+
+
+
 fn var_num(row: i16, column: i16, val: i16, board_size: i16) -> i16{
     (row-1) * board_size * board_size + (column-1) * board_size + val
 }
