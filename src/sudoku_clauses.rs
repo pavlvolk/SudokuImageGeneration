a7fb935f617893860837268fe422d8ebfe0a065f
paul <paulvolk.pv@gmail.com>
2025-04-22 22:18:14 +0200

    Grid clauses implemented

--- src/sudoku_clauses.rs
+++ src/sudoku_clauses.rs
@@ -76,7 +76,34 @@ pub fn add_column_clauses(clauses: &mut [i16], board_size: i16) {
     }
 }
 
-
+pub fn add_grid_clauses(clauses: &mut [i16], board_size: i16) {
+    let mut r_board_size:i16 = 0;
+    let mut c_board_size:i16 = 0;
+    match board_size {
+        4 => {r_board_size = 2; c_board_size = 2},
+        6 => {r_board_size = 2; c_board_size = 3},
+        9 => {r_board_size = 3; c_board_size = 3},
+        _ => println!("Invalid board size!")
+    }
+    for br in 0..c_board_size {
+        for bc in 0..r_board_size {
+            for val in 1..=board_size {
+                let mut block_clauses:Vec<i16> = vec![];
+                for r in 1 + br * r_board_size..1 + (br + 1) * r_board_size {
+                    for c in 1 + bc * c_board_size..1 + (bc + 1) * c_board_size {
+                        block_clauses.push(var_num(r, c, val, board_size))
+                    }
+                }
+                for i in 0..block_clauses.len() {
+                    for j in 0..block_clauses.len() {
+                        clauses.push(vec![-block_clauses[i], -block_clauses[j]]);
+                    }
+                }
+                clauses.push(block_clauses);
+            }
+        }
+    }
+}
 
 fn var_num(row: i16, column: i16, val: i16, board_size: i16) -> i16{
     (row-1) * board_size * board_size + (column-1) * board_size + val
