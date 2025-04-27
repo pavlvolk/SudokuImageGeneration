0fb9b133144bbd03631894370b957b035f1dca75
paul <paulvolk.pv@gmail.com>
2025-04-27 13:50:58 +0200

    Made some fn public and struct

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -2,14 +2,14 @@ use cadical::Solver;
 use sudoku_clauses::add_hint;
 use crate::sudoku_clauses;
 
-struct Sudoku{
+pub struct Sudoku{
     boardsize: i16,
     board: Vec<Vec<u8>>,
     standard_clauses:Vec<Vec<i16>>,
 }
 
 impl Sudoku {
-    fn new(board_size: i16, clauses: Vec<Vec<i16>>) -> Sudoku {
+    pub fn new(board_size: i16, clauses: Vec<Vec<i16>>) -> Sudoku {
         assert!(board_size == 4 || board_size == 6 || board_size == 9);
         let mut board = vec![vec![0; board_size as usize]; board_size as usize];
         Sudoku {
