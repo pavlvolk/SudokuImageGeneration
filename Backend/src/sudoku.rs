82101f9cc9f709cc19943cebc328b47a138fae89
Paul Volk <st183013@stud.uni-stuttgart.de>
2025-04-30 12:10:32 +0200

    optimized sudoku.rs

--- Backend/src/sudoku.rs
+++ Backend/src/sudoku.rs
@@ -1,29 +1,27 @@
 use cadical::Solver;
 use sudoku_clauses::add_hint;
 use crate::sudoku_clauses;
+use crate::sudoku_clauses::sudoku_clauses;
 
 pub struct Sudoku{
     board_size: i32,
-    board: Vec<Vec<i32>>,
     standard_clauses:Vec<Vec<i32>>,
 }
 
 impl Sudoku {
     pub fn new(board_size: i32, clauses: Vec<Vec<i32>>) -> Sudoku {
         assert!(board_size == 4 || board_size == 6 || board_size == 9);
-        let mut board = vec![vec![0; board_size as usize]; board_size as usize];
         Sudoku {
             board_size,
-            board,
-            standard_clauses: clauses
+            standard_clauses: sudoku_clauses(board_size)
         }
     }
 
-    pub fn unique(mut self, hints:Vec<i32>) -> bool{
+    pub fn unique(&mut self, hints:Vec<i32>) -> bool{
         let (solvable, possible_sol) = Self::solvable(&self, &hints);
         if solvable {
             let solution = possible_sol.unwrap();
-            let new_sudoku = self.add_hints(&hints);
+            let new_sudoku = Self::add_hints(&self, &hints);
             let mut clauses = new_sudoku.standard_clauses;
             let mut forbidden:Vec<i32> = Vec::new();
             for var in solution {
@@ -69,33 +67,12 @@ impl Sudoku {
         for i in 0..hints.len() {
             if hints[i] > 0 {
                 let (row, col) = Self::find_column_row(&self, i as i32);
-                standard_clauses = add_hint(&standard_clauses, hints[i], row, col, self.board_size);
+                add_hint(&mut standard_clauses, hints[i], row, col, self.board_size);
             }
         }
         Sudoku{
             board_size: self.board_size,
-            board: self.board.clone(),
             standard_clauses,
-        }
-    }
-
-    pub fn compute_solution(&mut self, solution:&Vec<i32>) {
-        let mut res = vec![vec![0; self.board_size as usize]; self.board_size as usize];
-        let size = self.board_size;
-        for v in solution {
-            let r = (v-1) / (size*size) + 1;
-            let c = ((v-1) % (size*size)) / size + 1;
-            let d = (v-1) % size + 1;
-            if 1 <= r && r <= size && 1 <= c && c <= size && 1 <= d && d <= size {
-                res[r as usize-1][c as usize-1] = d;
-            }
-        }
-        self.board = res;
-    }
-
-    pub fn print_solution(&self){
-        for r in 0..self.board_size {
-            println!("{:?}", self.board[r as usize]);
         }
     }
 }
