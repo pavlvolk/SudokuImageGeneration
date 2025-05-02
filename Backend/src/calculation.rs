fe62b02e90681f0beb7557827ac199364a99ad7b
Paul Volk <st183013@stud.uni-stuttgart.de>
2025-05-02 18:55:58 +0200

    added doc

--- Backend/src/calculation.rs
+++ Backend/src/calculation.rs
@@ -2,6 +2,15 @@ use crate::fill_grid::fill_grid;
 use crate::sudoku;
 use crate::sudoku::Sudoku;
 
+
+/**
+*   This function takes hints and a sudoku of a specific size and outputs a solution if there is a definite one.
+*   list and filled must agree if the list of hints is filled.
+*   @param list List of hints that are either filled with {0, 1} or with numbers 0,...,9.
+*   @param sudoku The sudoku structure of a given size
+*   @param filled boolean if the hints is filled with values or just representation if a cell is filled or not.
+*   @return Option<Vec<i32>> the solution of the sudoku if it is unique.
+*/
 pub fn calculate_solution(list: &Vec<usize>, sudoku: &mut Sudoku, filled:bool) -> Option<Vec<i32>> {
     if !filled {
         let (results, row_permutation, col_permutation, mirror) = fill_grid(&list, &(sudoku.board_size as usize));
