b5ace1dca642418d7ae1e084c67096320cb4eb1d
Bastian Fischer <bastifi@gmx.net>
2025-07-18 23:34:12 +0200

    Zahlen eingeben hinzugefügt und schnelles eingeben aktiviert

--- Backend/src/constants.rs
+++ Backend/src/constants.rs
@@ -5,6 +5,6 @@ pub const SOLUTION: &str = "data/permuted_10_million_cut_trimmed.txt";
 
 pub const TEST: &str = "data/unbiased_sudokus_formated.txt";
 
-pub static NUMBER_OF_THREADS: Lazy<usize> = Lazy::new(|| num_cpus::get_physical());
+pub static NUMBER_OF_THREADS: Lazy<usize> = Lazy::new(|| 6);
 
 pub const SOLUTION_PER_THREAD: usize = 10000;
